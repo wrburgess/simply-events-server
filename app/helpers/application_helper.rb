38b9a4082938e3508a9ee4344ebdabf6e0bc5f0c
Randy Burgess <wrburgess@gmail.com>
2016-08-12 11:04:59 -0500

    adding helper specs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def copyright_notice
+    "&copy; 2016 Access Cal"
+  end
+
+  def number_with_leading(integer)
+    integer.to_s.rjust(2, "0")
+  end
 end
+
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe ApplicationHelper, type: :helper do
+  describe "#copyright_notice" do
+    it "renders the current year in the Copyright notice" do
+      expect(copyright_notice).to eq "&copy; 2016 Access Cal"
+    end
+  end
+
+  describe "#number_with_leading" do
+    it "coverts integer into string with leading zero if necessary" do
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].each do |num|
+        expect(number_with_leading(num)).to eq "0#{num}"
+      end
+    end
+
+    it "does not covert multi-digit integers to strings with leading zero" do
+      [10, 100, 20_000, 300_000].each do |num|
+        expect(number_with_leading(num)).to eq num.to_s
+      end
+    end
+  end
+end
