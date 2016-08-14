e30311d800b84eaf1f02086c5f022dbdc32b71c0
Randy Burgess <wrburgess@gmail.com>
2016-08-14 09:14:39 -0500

    adding seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+puts "BEGIN: Create Admin"
+User.create(email: "admin@example.com", first_name: "John", last_name: "Doe")
+puts "END:   Create Admin"
+
+puts "BEGIN: Creating Users"
+users = []
+(1..200).each do
+  users << { email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name }
+end
+User.create(users)
+puts "END:   Creating Users"
+
 puts "BEGIN: Creating Events"
 events = []
 (1..200).each do
